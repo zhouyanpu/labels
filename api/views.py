a847ace4bf6b52344fe4cd10b28614471a8c3a05
Yanpu <zhouyanpu@gmail.com>
2023-08-21 18:13:04 -0400

    basic

--- api/views.py
+++ api/views.py
@@ -3,5 +3,6 @@ from rest_framework.decorators import api_view
 
 @api_view(['GET'])
 def getData(request):
-    person = {'name':'dennis', 'age':28}
+    person = {'name':'dennis', 'age':6666666888888}
+    print(request, '!!!!!dsads!!')
     return Response(person)
--- mylabels/settings.py
+++ mylabels/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'base',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'mylabels.urls'
@@ -124,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True 
