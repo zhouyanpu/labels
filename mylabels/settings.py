593a05822e7c524e841b67cdded782ad7b41551f
Yanpu <zhouyanpu@gmail.com>
2023-08-21 20:16:03 -0400

    allowed hosts

--- mylabels/settings.py
+++ mylabels/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)20taez9c20_$kz=xh02&%jtkws0##b$#p&$(oc^kb5#%f$^93
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.14.248.3']
 
 
 # Application definition
